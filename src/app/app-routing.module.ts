c5d2d793a1114e034e707f6f35ff9ba801cb2e01
jing <jing@caravan.coop>
2021-09-20 20:59:08 -0400

    set up tailwind, kickoff project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { ColorsComponent } from './components/colors/colors.component'
+import { TypographyComponent } from './components/typography/typography.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'colors', component: ColorsComponent },
+  { path: 'typography', component: TypographyComponent}
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ColorsComponent } from './components/colors/colors.component';
+import { TypographyComponent } from './components/typography/typography.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ColorsComponent,
+    TypographyComponent
   ],
   imports: [
     BrowserModule,
