417e6b7873ffc7fda2e7697e9c249b3bed79dc45
jing <jing@caravan.coop>
2021-10-01 23:50:04 -0400

    add dark mode switch, more feather icon examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component } from '@angular/core'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'uikit';
+  title = 'uikit'
+
+  darkMode = false
+
+  toggleDarkMode() {
+    document.documentElement.classList.toggle('dark')
+    this.darkMode = !this.darkMode
+  }
+
 }
